41213e0a82c3a9833c634cb9878d91ff0fb5341a
Adar Hilsenrat <50073577+hilsenrat@users.noreply.github.com>
2026-02-17 22:07:09 +0200

    Update src/app/layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "JOHN SMITH",
+  description: "I am a passionate creator who loves to build beautiful and functional things.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
